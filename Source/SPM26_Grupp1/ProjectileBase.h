c5e8ed981f6bdab969bec7ed3d2a82612651468a
kuronity <kurobjorn@gmail.com>
2026-04-14 12:08:20 +0200

    Abstract/Interface Weapon

---
+++ Source/SPM26_Grupp1/ProjectileBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class SPM26_GRUPP1_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category="Projectile")
+	class UProjectileMovementComponent* ProjectileMovement;
+	
+	UPROPERTY(VisibleAnywhere, Category="Projectile")
+	class USphereComponent* CollisionComp;
+	
+	UPROPERTY(VisibleAnywhere, Category="Projectile")
+	float InitialSpeed = 1000.f;
+
+};
