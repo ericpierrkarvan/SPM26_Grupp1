e571f0f501d2877e6b2afa074a8131845ecd138a
Emil <112473413+emilte21a@users.noreply.github.com>
2026-04-21 23:15:05 +0200

    Finished mantle!! behöver bara lägga till animationer och ta bort de temporära variablerna vi har just nu

--- Source/SPM26_Grupp1/Components/MechanicMovementComponent.cpp
+++ Source/SPM26_Grupp1/Components/MechanicMovementComponent.cpp
@@ -27,61 +27,71 @@ void UMechanicMovementComponent::UpdateCharacterStateBeforeMovement(float DeltaS
 
 void UMechanicMovementComponent::TryMantle()
 {
+	//Don't mantle if on the ground
 	if (IsGrounded())
 	{
 		return;
 	}
-
+	
+	//Compute cosine thresholds for angle checks
 	float MinWallSteepnessAngleCos = FMath::Cos(FMath::DegreesToRadians(MantleMinWallSteepnessAngle));
 	float MaxSurfaceAngleCos = FMath::Cos(FMath::DegreesToRadians(MantleMaxSurfaceAngle));
 	float MaxAlignmentAngleCos = FMath::Cos(FMath::DegreesToRadians(MantleMaxAlignmentAngle));
 	FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
-
+	
+	//Ignore the mechanic player
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(CharacterOwner);
 
-	FHitResult FrontResult;
+	//Trace checks to see if a wall is in front of the player
+	FHitResult WallDetectTraceResult;
 	FVector FrontStart = ComponentLocation + FVector::UpVector * MantleUpOffsetDistance;
 	FVector ForwardVector = UpdatedComponent->GetForwardVector().GetSafeNormal2D();
 	float CheckDistance = FMath::Clamp(Velocity | ForwardVector, GetCapsuleRadius() + 30.f,
 	                                   MaxFrontMantleCheckDistance);
 	FVector FrontEnd = FrontStart + ForwardVector * CheckDistance;
-
+	
 	DrawDebugLine(GetWorld(), FrontStart, FrontEnd, FColor::Red);
-	if (!GetWorld()->LineTraceSingleByProfile(FrontResult, FrontStart, FrontEnd, "BlockAll", QueryParams))
+	
+	//Perform forward trace
+	if (!GetWorld()->LineTraceSingleByProfile(WallDetectTraceResult, FrontStart, FrontEnd, "BlockAll", QueryParams))
 	{
 		return;
 	}
 
-	if (!FrontResult.IsValidBlockingHit())
+	if (!WallDetectTraceResult.IsValidBlockingHit())
 	{
 		return;
 	}
-
-	float CosWallSteepnessAngle = FrontResult.Normal | FVector::UpVector;
-	if (FMath::Abs(CosWallSteepnessAngle) > MinWallSteepnessAngleCos || (ForwardVector | -FrontResult.Normal) <
+	
+	//Check if wall is steep enough
+	float CosWallSteepnessAngle = WallDetectTraceResult.Normal | FVector::UpVector;
+	if (FMath::Abs(CosWallSteepnessAngle) > MinWallSteepnessAngleCos || (ForwardVector | -WallDetectTraceResult.Normal) <
 		MaxAlignmentAngleCos)
 	{
 		return;
 	}
 
-	DrawDebugPoint(GetWorld(), FrontResult.Location, 15, FColor::Green, false, 2.f);
-
+	DrawDebugPoint(GetWorld(), WallDetectTraceResult.Location, 15, FColor::Green, false, 2.f);
+	
+	//Find the top surface to mantle on
 	TArray<FHitResult> HeightHits;
 	FHitResult SurfaceHit;
-	FVector WallUp = FVector::VectorPlaneProject(FVector::UpVector, FrontResult.Normal).GetSafeNormal();
-	float WallCos = FVector::UpVector | FrontResult.Normal;
+	FVector WallUp = FVector::VectorPlaneProject(FVector::UpVector, WallDetectTraceResult.Normal).GetSafeNormal();
+	float WallCos = FVector::UpVector | WallDetectTraceResult.Normal;
 	float WallSin = FMath::Sqrt(1 - WallCos * WallCos);
 
-	FVector TraceStart = FrontResult.Location + ForwardVector + WallUp * MantleReachHeight / WallSin;
-	DrawDebugLine(GetWorld(), TraceStart, FrontResult.Location + ForwardVector, FColor::Black);
-
-	if (!GetWorld()->LineTraceMultiByProfile(HeightHits, TraceStart, FrontResult.Location + ForwardVector, "BlockAll",
+	FVector TraceStart = WallDetectTraceResult.Location + ForwardVector + WallUp * MantleReachHeight / WallSin;
+	DrawDebugLine(GetWorld(), TraceStart, WallDetectTraceResult.Location + ForwardVector, FColor::Black);
+	
+	//Perform multi trace to check all valid points to mantle onto
+	if (!GetWorld()->LineTraceMultiByProfile(HeightHits, TraceStart, WallDetectTraceResult.Location + ForwardVector, "BlockAll",
 	                                         QueryParams))
 	{
 		return;
 	}
-
+	
+	//Choose the first valid mantling point
 	for (const FHitResult& HitResult : HeightHits)
 	{
 		if (HitResult.IsValidBlockingHit())
@@ -90,26 +100,27 @@ void UMechanicMovementComponent::TryMantle()
 			break;
 		}
 	}
-
+	
+	//Check if the surface is flat enough
 	if (!SurfaceHit.IsValidBlockingHit() || (SurfaceHit.Normal | FVector::UpVector) < MaxSurfaceAngleCos)
 	{
 		return;
 	}
-
+	
 	float Height = SurfaceHit.Location - FrontStart | FVector::UpVector;
-
 	DrawDebugPoint(GetWorld(), SurfaceHit.Location, 20, FColor::Blue, false, 2.f);
-
+	
 	if (Height > MantleReachHeight)
 	{
 		return;
 	}
-
+	
+	//Get the position that the player will end up on
 	float SurfaceCos = FVector::UpVector | SurfaceHit.Normal;
 	float SurfaceSin = FMath::Sqrt(1 - SurfaceCos * SurfaceCos);
 	FVector TransitionTarget = SurfaceHit.Location + ForwardVector * GetCapsuleRadius() + FVector::UpVector * (
 		GetCapsuleHalfHeight() + 1 + GetCapsuleRadius() * 2 * SurfaceSin);
-
+	
 	FCollisionShape CapsuleShape = FCollisionShape::MakeCapsule(GetCapsuleRadius(), GetCapsuleHalfHeight());
 
 	if (GetWorld()->OverlapAnyTestByProfile(TransitionTarget, FQuat::Identity, "BlockAll", CapsuleShape, QueryParams))
@@ -123,22 +134,21 @@ void UMechanicMovementComponent::TryMantle()
 	DrawDebugCapsule(GetWorld(), TransitionTarget, GetCapsuleHalfHeight(), GetCapsuleRadius(), FQuat::Identity,
 	                 FColor::Green, false, 2.f);
 
+	//Override the rootmotion for a certain duration and make the character move to the target location
 	RootMotionSource.Reset();
-
 	RootMotionSource = MakeShared<FRootMotionSource_MoveToForce>();
-
 	RootMotionSource->AccumulateMode = ERootMotionAccumulateMode::Override;
 	RootMotionSource->Duration = 0.5f; //MantleAnimation->GetPlayLength();
 	RootMotionSource->StartLocation = ComponentLocation;
 	RootMotionSource->TargetLocation = TransitionTarget;
-	
 	Acceleration = FVector::ZeroVector;
 	Velocity = FVector::ZeroVector;
-	
 	SetMovementMode(EMovementMode::MOVE_Flying);
 	RootMotionSourceID = ApplyRootMotionSource(RootMotionSource);
-	
+
 	CharacterOwner->PlayAnimMontage(MantleAnimation);
+	//Temoporär fix tills att vi får en animation
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UMechanicMovementComponent::OnMantleAnimFinished, 0.5f, false);
 }
 
 void UMechanicMovementComponent::OnMantleAnimFinished()
