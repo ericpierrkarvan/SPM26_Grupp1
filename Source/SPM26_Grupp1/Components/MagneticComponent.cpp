3cdc8f31a6ca7b46f26ec0a41758a40d6f14bd20
kuronity <kurobjorn@gmail.com>
2026-05-04 13:34:54 +0200

    First draft MagneticComponent-class

---
+++ Source/SPM26_Grupp1/Components/MagneticComponent.cpp
@@ -0,0 +1,46 @@
+#include "MagneticComponent.h"
+
+
+UMagneticComponent::UMagneticComponent()
+{
+	Polarity = EPolarity::Positive;
+	bIsActive = true;	
+	bCanBeAffected = true;
+	ResistanceFactor = 1.0f;
+}
+
+void UMagneticComponent::SwitchPolarity()
+{
+	Polarity == EPolarity::Positive ? Polarity = EPolarity::Positive : Polarity = EPolarity::Negative;
+}
+
+EPolarity UMagneticComponent::GetPolarity() const
+{
+	return Polarity;
+}
+
+// Returns if the magnetic component can be affected by magnetism.
+bool UMagneticComponent::CanBeAffected() const
+{
+	return bCanBeAffected;
+}
+
+float UMagneticComponent::GetResistanceFactor() const
+{
+	return ResistanceFactor;
+}
+
+void UMagneticComponent::SetPolarity(const EPolarity Polarity)
+{
+	this->Polarity = Polarity;
+}
+
+void UMagneticComponent::SetCanBeAffected(const bool bCanBeAffected)
+{
+	this->bCanBeAffected = bCanBeAffected;
+}
+
+void UMagneticComponent::SetResistanceFactor(const float ResistanceFactor)
+{
+	this->ResistanceFactor = ResistanceFactor;
+}
---
+++ Source/SPM26_Grupp1/Components/MagneticComponent.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "SPM26_Grupp1/Enum/Polarity.h"
+
+UCLASS()
+class UMagneticComponent : public UActorComponent
+{
+public:
+	UMagneticComponent();
+	
+	void SwitchPolarity();
+
+	// Getters & Setters
+	EPolarity GetPolarity() const;
+	float GetResistanceFactor() const;
+	void SetPolarity(EPolarity Polarity);
+	void SetCanBeAffected(bool bCanBeAffected);
+	void SetResistanceFactor(float ResistanceFactor);
+	
+	bool CanBeAffected() const;
+
+private:
+	UPROPERTY(EditAnywhere, Category="MagneticComponent")
+	EPolarity Polarity;
+	
+	UPROPERTY(EditAnywhere, Category="MagneticComponent")
+	bool bIsActive;
+	
+	UPROPERTY(EditAnywhere, Category="MagneticComponent")
+	bool bCanBeAffected;
+	
+	// Resistance to being moved by magnetic fields
+	float ResistanceFactor;
+	
+
+};
