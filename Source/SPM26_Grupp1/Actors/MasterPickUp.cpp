76f518c1fed65169d5227f8f5a3749fa49419c3b
ericpierrkarvan <epkepk@gmail.com>
2026-05-06 22:36:39 +0200

    code class for pickup objects

---
+++ Source/SPM26_Grupp1/Actors/MasterPickUp.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPM26_Grupp1/Actors/MasterPickUp.h"
+#include "Components/StaticMeshComponent.h"
+
+#include "SPM26_Grupp1/Components/PickupComponent.h"
+
+// Sets default values
+AMasterPickUp::AMasterPickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComponent);
+	
+	PickupComponent = CreateDefaultSubobject<UPickupComponent>(TEXT("PickupComp"));
+}
+
+// Called when the game starts or when spawned
+void AMasterPickUp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMasterPickUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SPM26_Grupp1/Actors/MasterPickUp.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MasterPickUp.generated.h"
+
+class UPickupComponent;
+
+UCLASS()
+class SPM26_GRUPP1_API AMasterPickUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMasterPickUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, Category="Pickup")
+	UPickupComponent* PickupComponent;
+
+	UPROPERTY(VisibleAnywhere, Category="Pickup")
+	UStaticMeshComponent* MeshComponent;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
