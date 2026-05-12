cb2a57f668753b3fdc25d4f64c059906b2cc4697
kuronity <kurobjorn@gmail.com>
2026-05-12 22:19:38 +0200

    AI behaviourtree, blackboard etc

--- Source/SPM26_Grupp1/AI/AlienAIController.cpp
+++ Source/SPM26_Grupp1/AI/AlienAIController.cpp
@@ -3,29 +3,50 @@
 
 #include "AlienAIController.h"
 
+#include "AssetDefinitionAssetInfo.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 void AAlienAIController::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	if (AIBehavior)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AAlienAIController::BeginPlay() BehaviorTree: %s"), *AIBehavior->GetName());
+		RunBehaviorTree(AIBehavior);
+		
+		APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		APawn *NPCPawn = GetPawn();
+		UE_LOG(LogTemp, Warning, TEXT("NPC velocity: %s"), *NPCPawn->GetVelocity().ToCompactString());
+		BBC = GetBlackboardComponent();
+		BBC->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		BBC->SetValueAsVector(TEXT("StartLocation"), NPCPawn->GetActorLocation());
+	}
+
 }
 
 void AAlienAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 	if (!PlayerPawn) return;
+	//UE_LOG(LogTemp, Warning, TEXT("NPC velocity: %s"), *GetPawn()->GetVelocity().ToCompactString());
 	// UE_LOG(LogTemp, Warning, TEXT("AlienNPCCharacter(): Character is: %s"), *PlayerPawn->GetName());
 	
 	if (LineOfSightTo(PlayerPawn))
 	{
-		MoveToActor(PlayerPawn, AcceptanceRadius);
-		SetFocus(PlayerPawn);
+		UE_LOG(LogTemp, Warning, TEXT("LineOfSightTo() Player's ActorLocation: %s"), *PlayerPawn->GetActorLocation().ToCompactString());
+		BBC->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		BBC->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
 	}
 	else
 	{
-		ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();
+		// clear PlayerLocation
+		BBC->ClearValue("PlayerLocation");
 	}
+	
 
 }
--- Source/SPM26_Grupp1/AI/AlienAIController.h
+++ Source/SPM26_Grupp1/AI/AlienAIController.h
@@ -21,7 +21,19 @@ public:
 private:
 	
 	// Distance to player where AI stops chasing
-	UPROPERTY(EditAnywhere, Category="AI")
-	float AcceptanceRadius = 200;
+	//UPROPERTY(EditAnywhere, Category="AI")
+	//float AcceptanceRadius = 200;
+	
+	UPROPERTY(EditAnywhere)
+	UBehaviorTree* AIBehavior;
+	
+	UPROPERTY(EditAnywhere)
+	UBlackboardComponent* BBC;
+	
+	UPROPERTY(EditAnywhere)
+	float NPCSlowMovementSpeed = 200;
+	UPROPERTY(EditAnywhere)
+	float NPCFastMovementSpeed = 400;
+	
 	
 };
