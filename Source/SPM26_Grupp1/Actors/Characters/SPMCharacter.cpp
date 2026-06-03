b9f8b5c48ebfb4e81bcf079772f4913151aeb4df
ericpierrkarvan <epkepk@gmail.com>
2026-06-03 16:01:08 +0200

    cutscenes: support for lvl2 end, endcredits

--- Source/SPM26_Grupp1/Actors/Characters/SPMCharacter.cpp
+++ Source/SPM26_Grupp1/Actors/Characters/SPMCharacter.cpp
@@ -334,7 +334,7 @@ void ASPMCharacter::HandleGeneratorQuestFlags(const UProgressSubsystem* Progress
 	if (Progress->GetGeneratorFlagsUnlocked() == 3)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("YOU WIN GAME! Sydney Sweeney is on the phone"));
-		TSoftObjectPtr<UWorld> StartMenuLevel(FSoftObjectPath(TEXT("/Game/Levels/Startmenue.Startmenue")));
+		TSoftObjectPtr<UWorld> StartMenuLevel(FSoftObjectPath(TEXT("/Game/Levels/Cutscenes/Level_Level2_End.Level_Level2_End")));
 		USPMGameInstance* GI = Cast<USPMGameInstance>(UGameplayStatics::GetGameInstance(this));
 		GI->LoadLevel(StartMenuLevel, true);
 	}	
